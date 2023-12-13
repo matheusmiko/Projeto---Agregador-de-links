df8818ab95a84d4e958614c196a3d336ad6a072d
matheusmiko <matheusmiko33@gmail.com>
2023-12-13 18:59:53 -0300

    finalizacao

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function toggleMode() {
 
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-sun.png")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar-moon.png")
   }
 }
